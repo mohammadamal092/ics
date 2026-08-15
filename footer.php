a1dcb05e03c51b404af4c47b2979a55c47580789
mohammadamal092 <mohammadamal092@gmail.com>
2026-08-15 21:34:47 +0700

    Delete footer.php

--- footer.php
+++
@@ -1,7 +0,0 @@
-</main>
-<footer class="footer">
-    <p>&copy; <?= date('Y') ?> Inventory Control System - Tugas Kuliah</p>
-</footer>
-<script src="/assets/js/script.js"></script>
-</body>
-</html>
